5954cc8efbde2b9941cad1ad4568c78e7c49e23f
larry.adams <ladams1776@yahoo.com>
2019-09-14 08:10:04 -0400

    Create Controller

--- src/main/java/com/microservices/currencyconversionservice/CunrrencyConverterController.java
+++ src/main/java/com/microservices/currencyconversionservice/CunrrencyConverterController.java
@@ -1,4 +1,19 @@
 package com.microservices.currencyconversionservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CunrrencyConverterController {
+
+    @GetMapping("/convert-currency/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionValueObject convertCurrency(@PathVariable String from,
+                                                         @PathVariable String to,
+                                                         @PathVariable BigDecimal quantity) {
+        return new CurrencyConversionValueObject(from, to, BigDecimal.ONE, BigDecimal.ONE, quantity, 0);
+    }
 }
